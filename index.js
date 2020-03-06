599ad68f2606119f7c654b9dfa6d925da7bcd70d
Alexandru Kis <alex@sectorlabs.ro>
2020-03-06 13:58:35 +0200

    Only update the line-height if the user explicitely asked for it.

--- index.js
+++ index.js
@@ -3,76 +3,79 @@ var units = require('units-css');
 var parseCssFont = require('parse-css-font');
 var assign = require('object-assign');
 
-module.exports = postcss.plugin('postcss-increase-text-size', function (opts) {
-	opts = assign({
-		fontSizeMultiplyBy: 1.2,
-		lineheightMultiplyBy: 1.2,
-		selectorsBlackList: []
-	}, opts);
+module.exports = postcss.plugin('postcss-increase-text-size', function(opts) {
+    opts = assign(
+        {
+            fontSizeMultiplyBy: 1.2,
+            selectorsBlackList: [],
+        },
+        opts,
+    );
 
-	function propInArray(array, prop) {
-		return array.indexOf(prop) > -1;
-	}
+    function propInArray(array, prop) {
+        return array.indexOf(prop) > -1;
+    }
 
-	return function (css, result) {
-		css.walkDecls(function (decl) {
+    return function(css, result) {
+        css.walkDecls(function(decl) {
+            if (propInArray(opts.selectorsBlackList, decl.parent.selector)) {
+                return;
+            }
 
-			if (propInArray(opts.selectorsBlackList, decl.parent.selector)) {
-				return;
-			}
+            var shouldUpdateLineHeight = !!opts.lineHeightMultiplyBy;
+            if (decl.prop === 'font') {
+                var fontValue = parseCssFont(decl.value);
+                var fontSize = units.parse(fontValue.size);
+                var lineHeight = fontValue.lineHeight;
+                var fontValueCon;
 
-			if (decl.prop === 'font') {
+                if (fontSize.unit === 'em' || fontSize.unit === 'rem') {
+                    fontSize = fontSize.value * opts.fontSizeMultiplyBy + fontSize.unit;
+                } else {
+                    fontSize = Math.round(fontSize.value * opts.fontSizeMultiplyBy) + fontSize.unit;
+                }
 
-				var fontValue = parseCssFont(decl.value);
-				var fontSize = units.parse(fontValue.size);
-				var lineHeight = fontValue.lineHeight;
-				var fontValueCon;
+                if (lineHeight !== 'normal' && shouldUpdateLineHeight) {
+                    var newlineHeight = units.parse(lineHeight);
 
-				if (fontSize.unit == 'em' || fontSize.unit == 'rem') {
-					fontSize = fontSize.value * opts.fontSizeMultiplyBy + fontSize.unit;
-				} else {
-					fontSize = Math.round(fontSize.value * opts.fontSizeMultiplyBy) + fontSize.unit;
-				}
+                    if (newlineHeight.unit === 'em' || newlineHeight.unit === 'rem') {
+                        newlineHeight =
+                            newlineHeight.value * opts.lineHeightMultiplyBy + newlineHeight.unit;
+                    } else {
+                        newlineHeight =
+                            Math.round(newlineHeight.value * opts.lineHeightMultiplyBy) +
+                            newlineHeight.unit;
+                    }
 
+                    fontValueCon = fontSize + '/' + newlineHeight;
+                } else {
+                    fontValueCon = fontSize;
+                }
 
-				if (lineHeight != 'normal') {
-					var newlineHeight = units.parse(lineHeight);
+                decl.value = decl.value.replace(/(\d\S*)(\d+)?/g, fontValueCon);
+            }
 
-					if (newlineHeight.unit == 'em' || newlineHeight.unit == 'rem') {
-						newlineHeight = newlineHeight.value * opts.lineheightMultiplyBy + newlineHeight.unit;
-					} else {
-						newlineHeight = Math.round(newlineHeight.value * opts.lineheightMultiplyBy) + newlineHeight.unit;
-					}
+            if (decl.prop === 'font-size') {
+                var propUnit = units.parse(decl.value);
 
-					fontValueCon = fontSize + '/' + newlineHeight;
-				} else {
-					fontValueCon = fontSize;
-				}
+                if (propUnit.unit === 'em' || propUnit.unit === 'rem') {
+                    propUnit.value *= opts.fontSizeMultiplyBy;
+                } else {
+                    propUnit.value = Math.round(propUnit.value * opts.fontSizeMultiplyBy);
+                }
 
-				decl.value = decl.value.replace(/(\d\S*)(\d+)?/g, fontValueCon);
-			}
+                decl.value = propUnit.value + propUnit.unit;
+            }
 
-			if (decl.prop === 'font-size') {
-				var propUnit = units.parse(decl.value);
-
-				if (propUnit.unit == 'em' || propUnit.unit == 'rem') {
-					propUnit.value *= opts.fontSizeMultiplyBy;
-				} else {
-					propUnit.value = Math.round(propUnit.value * opts.fontSizeMultiplyBy);
-				}
-
-				decl.value = propUnit.value + propUnit.unit;
-			}
-
-			if (decl.prop === 'line-height') {
-				var propUnit = units.parse(decl.value);
-				if (propUnit.unit == 'em' || propUnit.unit == 'rem') {
-					propUnit.value *= opts.lineheightMultiplyBy;
-				} else {
-					propUnit.value = Math.round(propUnit.value * opts.lineheightMultiplyBy);
-				}
-				decl.value = propUnit.value + propUnit.unit;
-			}
-		});
-	};
+            if (decl.prop === 'line-height' && shouldUpdateLineHeight) {
+                var propUnit = units.parse(decl.value);
+                if (propUnit.unit === 'em' || propUnit.unit === 'rem') {
+                    propUnit.value *= opts.lineHeightMultiplyBy;
+                } else {
+                    propUnit.value = Math.round(propUnit.value * opts.lineHeightMultiplyBy);
+                }
+                decl.value = propUnit.value + propUnit.unit;
+            }
+        });
+    };
 });
